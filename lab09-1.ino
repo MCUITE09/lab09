4c6d04d2a524b950cd7e51c02b803db40e3d049b
MCUITE09 <jason0975243297@gmail.com>
2017-06-05 20:42:49 +0800

    Add files via upload

---
+++ lab09-1.ino
@@ -0,0 +1,27 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+#include <Ultrasonic.h>
+#define TRIGGER_PIN 8
+#define ECHO_PIN 13
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+void setup() 
+{
+Serial.begin(9600);
+}
+void loop()
+{
+float cmMsec, inMsec;
+long microsec = ultrasonic.timing();
+cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
+Serial.print("MS: "); 
+Serial.print(microsec);
+Serial.print(", CM: ");
+Serial.print(cmMsec);
+Serial.print(", IN: "); 
+Serial.println(inMsec);
+lcd.print("CM:");
+lcd.print(cmMsec);
+delay(1000);
+lcd.clear();
+}
